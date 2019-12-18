041a7722b88e264c42d7d1cd6e18429388534ffd
SCcagg5 <eliot@LAPTOP-C7G3T243.localdomain>
2019-12-18 11:20:44 +0100

    src

---
+++ autolog.php
@@ -0,0 +1,40 @@
+<?php
+
+$secret = "1q2W3e4R";
+
+function verify_jwt($jwt, $secret){
+        $header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
+        $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+
+        $res = explode('.', $jwt);
+        if (count($res) != 3)
+                return false;
+
+        $signature = hash_hmac('sha256', $base64UrlHeader . "." . $res[1], $secret, true);
+        $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+        if ($base64UrlSignature != $res[2])
+                return false;
+        $payload = json_decode(base64_decode($res[1]), true);
+
+        if ($payload["time"] < time())
+                return false;
+
+        return $payload["user"];
+}
+
+if (isset($_GET["jwt"])) {
+        $res = verify_jwt($_GET["jwt"], $secret);
+        if ($res != false){
+		$username = $res;
+		$user = get_user_by('login', $username );
+
+		if ( !is_wp_error( $user ) )
+		{
+    			wp_clear_auth_cookie();    wp_set_current_user ( $user->ID );
+    			wp_set_auth_cookie  ( $user->ID );
+		}
+	}
+	$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+	$link = explode('?', $actual_link)[0];
+	header('Location: '.$link); exit;
+}
---
+++ jwt.php
@@ -0,0 +1,39 @@
+<?php 
+
+function get_jwt($email, $secret, $time = 1) {
+	$time = time() + ($time);
+	$header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
+	$payload = json_encode(['user' => $email, 'time' => $time]);
+	$base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+	$base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+	$signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $secret, true);
+	$base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+	$jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+	return $jwt;
+}
+
+function verify_jwt($jwt, $secret){
+	$header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
+	$base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+
+	$res = explode('.', $jwt);
+	if (count($res) != 3)
+		return false;
+
+	$signature = hash_hmac('sha256', $base64UrlHeader . "." . $res[1], $secret, true);
+	$base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+	if ($base64UrlSignature != $res[2])
+		return false;
+	$payload = json_decode(base64_decode($res[1]), true);
+
+	if ($payload["time"] < time())
+		return false;
+
+	return $payload["user"];
+}
+
+$secret = "1q2W3e4R";
+$jwt = get_jwt("_hublet@orange.fr", $secret, 1000);
+echo $jwt;
+//$res = verify_jwt($jwt, $secret);
+//echo $res;
